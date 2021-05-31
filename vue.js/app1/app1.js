b5645b03762167825f1d4d3902e890a1973e8020
coldDayss <80840462+coldDayss@users.noreply.github.com>
2021-05-31 11:35:11 +0900

    Add files via upload

---
+++ vue.js/app1/app1.js
@@ -0,0 +1,11 @@
+Vue.component('num-btn', { //자식 컴포넌트
+    template: '<button v-on:click="number++">{{ number }}</button>',
+    props: ['number'], //부모의 컴포넌트 값 1,2,3이 number로 전송됨
+    data: function(){
+        return {}
+    }
+});
+
+new Vue({
+    el: '#app1'
+});
---
+++ vue.js/app2/app2.js
@@ -0,0 +1,23 @@
+Vue.component('anchor', { //자식 컴포넌트
+    template: '<span v-on:click="go()">{{ text }}</span>',
+    props: ['text', 'href', 'openNew'], //부모의 컴포넌트 값 1,2,3이 text로 전송됨
+    data: function(){
+        return {}
+    },
+    methods: {
+        go: function() {
+            if(this.href){
+                if(this.openNew){
+                    window.open(this.href, '');
+                }
+                else{
+                    location.href=this.href;
+                }
+            }
+        }
+    }
+});
+
+new Vue({
+    el: '#app2'
+});
